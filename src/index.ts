cf2322f4bbd238cf52e21eff85bfeef2d242782d
Naveen kumar <naveenkumar000568@gmail.com>
2025-02-04 01:48:03 +0530

    Prisma setup done

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
+import { prismaClient } from "./lib/db";
 
 
 async function init() {
@@ -16,12 +17,32 @@ async function init() {
                 hello: String
                 say(name: String): String
             }
+            
+            type Mutation {
+                createUser(firstName: String!, lastName: String!, email: String!, password: String!): Boolean
+            }
         `,//schema
         resolvers: {
             Query: {
                 hello: () => `Hey there, i am graphql server!`,
-                say: (_, {name}: {name: string} ) => `Hey ${name}, how are you?`
+                say: (_, { name }: { name: string }) => `Hey ${name}, how are you?`
             },
+
+            Mutation: {
+                createUser: async (_,
+                    { firstName, lastName, email, password }: { firstName: string; lastName: string; email: string; password: string; }) => {
+                    await prismaClient.user.create({
+                        data: {
+                            email,
+                            firstName,
+                            lastName,
+                            password,
+                            salt: "randoom_salt",
+                        },
+                    });
+                    return true;
+                }
+            }
         },
     });
 
